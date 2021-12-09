741f72523a61875185a7ec4845ed3191cb594c62
Emanuel Angelo <emanuelangelo@outlook.com.br>
2021-12-09 19:50:56 -0300

    implementando o react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import './App.css';
-import { useState } from 'react'
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/Saudacao';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
+import Contato from './pages/Contato';
+import Empresa from './pages/Empresa';
+import Home from './pages/Home';
 
 function App() {
-  const [nome, setNome] = useState()
-
-  return (
-    <div className="App">
-      <h1> State Lift </h1>
-      <SeuNome setNome={setNome} />
-      <Saudacao nome={nome} />
-     </div>
-  );
+   return (
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/">
+          <Home/>  
+        </Route>
+        <Route path="/empresa">
+          <Empresa/>  
+        </Route>
+        <Route path="/contato">
+          <Contato/>  
+        </Route>
+      </Switch>
+      <Footer/>
+    </Router>
+  )
 }
 
 export default App;
